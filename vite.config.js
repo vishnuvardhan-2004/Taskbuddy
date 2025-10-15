9d72eeba6832c5e2b72048b090f7fd1c1490f527
GangavarapuVishnuVardhan <vishnuvardhangang@gmail.com>
2025-10-15 19:17:01 +0530

    Fourth commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+base: '/Taskbuddy/',
   plugins: [react()],
-  base: '/Taskbuddy/', //Repo name
-})
+});
